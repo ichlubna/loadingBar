e11f0cfeda6f3f5c476f7925baf3ff4f45329e13
ichlubna <ichlubna@fit.vutbr.cz>
2022-08-03 10:02:28 +0200

    Getting rid of warnings

--- Makefile
+++ Makefile
@@ -1,2 +1,2 @@
 test: test.cpp loadingbar.hpp
-	g++ -std=c++17 -o test test.cpp loadingbar.hpp
+	g++ -std=c++17 -Wall -pedantic -o test test.cpp loadingbar.hpp
--- loadingbar.hpp
+++ loadingbar.hpp
@@ -5,7 +5,7 @@
 class LoadingBar
 {
     public:
-    LoadingBar(size_t total, bool printWidthAdd=true, bool printOnSameLine=true, size_t printStepSize=1) : totalCount{total}, addPrint{printWidthAdd}, stepSize{printStepSize}
+    LoadingBar(size_t total, bool printWidthAdd=true, bool printOnSameLine=true, size_t printStepSize=1) : totalCount{total}, stepSize{printStepSize}, addPrint{printWidthAdd} 
     {
         if(!printOnSameLine)
         {
@@ -23,8 +23,8 @@ class LoadingBar
 
     private:
     size_t totalCount;
+    size_t stepSize;
     size_t count{0};
-    size_t stepSize{1};
     bool addPrint{false};
     char lineStart = '\r';
     char lineEnd = '\r';
